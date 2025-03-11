57420ca3f8e1ec0fdd6ae3eb0b9f7351d67d456d
rijalaradhana <rijalaradhana@gmail.com>
2025-03-11 22:01:13 +0545

    few changes asti ko forgot to comit

--- bpms/change-password.php
+++ bpms/change-password.php
@@ -79,7 +79,6 @@ return true;
                 
  Change Password
             </h3>
-            <p class="tiltle-para ">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic fuga sit illo modi aut aspernatur tempore laboriosam saepe dolores eveniet.</p>
         </div>
 </div>
 </div>
